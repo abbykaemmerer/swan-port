c9f58c2f7dcf1edd29319849a405d25a6967f588
Abby Kaemmerer <abby.kaemmerer@twisthink.com>
2022-08-11 17:24:40 -0500

    More css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'swan-port';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+
+    // TODO:  more efficient way of changing background colors
+    // const boxes = Array.from(document.getElementsByClassName('box') as HTMLCollectionOf<HTMLElement>);
+    // boxes.forEach(box => {
+    //   box.addEventListener("mouseover", function(event) {
+    //     if (rectangle1 != null){
+    //       rectangle1.style.backgroundColor = 'blue';
+    //     }
+    //   })
+    // })
+
+    const rectangle1 = document.getElementById('rectangle1');
+    const rectangle2 = document.getElementById('rectangle2');
+    const rectangle3 = document.getElementById('rectangle3');
+    const rectangle4 = document.getElementById('rectangle4');
+    const rectangle5 = document.getElementById('rectangle5');
+
+    if (rectangle1 != null){
+      rectangle1?.addEventListener("mouseover", function(event) {
+        rectangle1.style.backgroundColor = '#f3e9dc';
+        rectangle1.style.transition = '.5s';
+    })
+      rectangle2?.addEventListener("mouseover", function(event) {
+          rectangle1.style.backgroundColor = '#faa007';
+      })
+      rectangle3?.addEventListener("mouseover", function(event) {
+        rectangle1.style.backgroundColor = '#e29d99';
+      })
+      rectangle4?.addEventListener("mouseover", function(event) {
+        rectangle1.style.backgroundColor = '#95bfc0';
+
+      })
+      rectangle5?.addEventListener("mouseover", function(event) {
+        rectangle1.style.backgroundColor = '#fec876';
+      })
+    } 
+  }
+
 }
+
+
+
+
+
+
